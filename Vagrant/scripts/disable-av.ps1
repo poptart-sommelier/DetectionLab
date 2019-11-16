74e69a42cce9f98a2d2a59213532dfc5ec52fc3c
poptart-sommelier <rich.d.nospam@gmail.com>
2019-11-15 23:00:10 -0500

    disabled redteam stuff, fixed av bug, added one more reload after av

--- Vagrant/scripts/disable-av.ps1
+++ Vagrant/scripts/disable-av.ps1
@@ -3,7 +3,7 @@ $hostname = $(hostname)
 
 If ($hostname -eq "win10") {
   # Disable Windows Defender for Win10
-  Set-MpPreference -DisableRealtimeMonitoring $true
+  # Set-MpPreference -DisableRealtimeMonitoring $true
   New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
 }
 
